696252fdb3d2ecb7bc311508b49ac8802b22b997
maxgamesNL <72696183+maxgamesNL@users.noreply.github.com>
2024-10-07 22:27:12 +0200

    informatica site

--- games/balls.js
+++
@@ -1,8 +0,0 @@
-var audio = new Audio('assets/songskibedie.mp3');
-var audioisplaying = false;
-addEventListener("click", (event) => {
-    if(!audioisplaying){
-        audio.play();
-        audioisplaying = true;
-    }
-});
